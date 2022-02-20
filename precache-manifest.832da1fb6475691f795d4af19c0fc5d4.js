477985890466afff75fe8f01a3ed167442239027
Dineshkumar <dineshkumar@crayondata.com>
2022-02-21 00:09:20 +0530

    Updates

---
+++ precache-manifest.832da1fb6475691f795d4af19c0fc5d4.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66e7d1556de4b5108a83800459c555b3",
+    "url": "/portfoliov2/index.html"
+  },
+  {
+    "revision": "8f3426d21035684e5702",
+    "url": "/portfoliov2/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "801177ef6616062c99a8",
+    "url": "/portfoliov2/static/css/main.48ce2d52.chunk.css"
+  },
+  {
+    "revision": "8f3426d21035684e5702",
+    "url": "/portfoliov2/static/js/2.793b7f48.chunk.js"
+  },
+  {
+    "revision": "801177ef6616062c99a8",
+    "url": "/portfoliov2/static/js/main.c2a884e7.chunk.js"
+  },
+  {
+    "revision": "d26f7f1865ac6d5c3c1f",
+    "url": "/portfoliov2/static/js/runtime~main.20d58baa.js"
+  },
+  {
+    "revision": "cfcb409306c533c39bdafca793cd6bbc",
+    "url": "/portfoliov2/static/media/Resume.cfcb4093.pdf"
+  },
+  {
+    "revision": "10523706cd80abdb3e58e49566334006",
+    "url": "/portfoliov2/static/media/cassandra_icon.10523706.png"
+  },
+  {
+    "revision": "c3679f3a16e88e13d4190b974df1cc9b",
+    "url": "/portfoliov2/static/media/cgpa_logo.c3679f3a.png"
+  },
+  {
+    "revision": "2d733718bd4cb4db180f3cfb42fb9bb8",
+    "url": "/portfoliov2/static/media/college.2d733718.png"
+  },
+  {
+    "revision": "87e9858a638bdc761f3de8e8a00ea057",
+    "url": "/portfoliov2/static/media/css_icon.87e9858a.png"
+  },
+  {
+    "revision": "d940468ea54dfa49571e4817fd45d991",
+    "url": "/portfoliov2/static/media/engineer.d940468e.png"
+  },
+  {
+    "revision": "4eab660bcc18dcaea8326998d62e481a",
+    "url": "/portfoliov2/static/media/github.4eab660b.png"
+  },
+  {
+    "revision": "2d66bb13635821e32362f4f9b5ba3647",
+    "url": "/portfoliov2/static/media/graduated.2d66bb13.png"
+  },
+  {
+    "revision": "74e943d82452f81f6a0bb0b51ac6785d",
+    "url": "/portfoliov2/static/media/java_icon.74e943d8.png"
+  },
+  {
+    "revision": "d9de75e5bf3a7b0ef2a48321acc06e88",
+    "url": "/portfoliov2/static/media/javascript_icon.d9de75e5.png"
+  },
+  {
+    "revision": "94db7e92e51707073eea9d740f69e975",
+    "url": "/portfoliov2/static/media/linkedin.94db7e92.png"
+  },
+  {
+    "revision": "818a5de6d1fd361e8f902e94fd17a631",
+    "url": "/portfoliov2/static/media/naukri.818a5de6.png"
+  },
+  {
+    "revision": "7e2b3c91203a8a907772484ae1bd9afb",
+    "url": "/portfoliov2/static/media/profile.7e2b3c91.jpg"
+  },
+  {
+    "revision": "be58231b3ed8d4b8e6b67751b213c568",
+    "url": "/portfoliov2/static/media/programing.be58231b.png"
+  },
+  {
+    "revision": "7cc08592c7c252040658bdf34bf66e97",
+    "url": "/portfoliov2/static/media/redux_icon.7cc08592.png"
+  },
+  {
+    "revision": "d654bb8086360fe29f324aa2ca191ad5",
+    "url": "/portfoliov2/static/media/resume_icon.d654bb80.png"
+  },
+  {
+    "revision": "613a6f1b8845ba51fb690deb53d46596",
+    "url": "/portfoliov2/static/media/school.613a6f1b.png"
+  },
+  {
+    "revision": "f6e54d53710dcf3b62e35925800f0d9c",
+    "url": "/portfoliov2/static/media/webDev.f6e54d53.png"
+  }
+]);
